7034e2a21a740004e53f7ff4345793353e8eb13f
AntonBuivol <120181261+AntonBuivol@users.noreply.github.com>
2023-11-13 20:29:46 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -35,19 +35,6 @@ namespace Laborotornaya_rabota
             ToolStripMenuItem Save = new ToolStripMenuItem("Save",save);
             ToolStripMenuItem Exit = new ToolStripMenuItem("Exit",exit);
 
-            File.DropDownItems.Add(New);
-            File.DropDownItems.Add(Open);
-            File.DropDownItems.Add(Save);
-            File.DropDownItems.Add(Exit);
-            ms.Items.Add(File);
-
-            New.ShortcutKeys = Keys.Control | Keys.N;
-            Open.ShortcutKeys = Keys.F3;
-            Save.ShortcutKeys = Keys.F2;
-            Exit.ShortcutKeys = Keys.Alt | Keys.X;
-
-            Save.Click += Save_Click;
-
             Image undo = new Bitmap("../../../Undo.png");
             Image redo = new Bitmap("../../../redo.png");
             Image pen = new Bitmap("../../../pen.png");
@@ -56,34 +43,49 @@ namespace Laborotornaya_rabota
             ToolStripMenuItem Edit = new ToolStripMenuItem("Edit");
             ToolStripMenuItem Undo = new ToolStripMenuItem("Undo", undo);
             ToolStripMenuItem Redo = new ToolStripMenuItem("Redo", redo);
-            Edit.DropDownItems.Add(Undo);
-            Edit.DropDownItems.Add(Redo);
-            Undo.ShortcutKeys = Keys.Control | Keys.Z;
-            Undo.Click += Undo_Click;
-            Redo.ShortcutKeys = Keys.Control | Keys.Shift | Keys.Z;
-            ToolStripMenuItem Pen = new ToolStripMenuItem("Pen",pen);
-            Edit.DropDownItems.Add(Pen);
-
+            ToolStripMenuItem Pen = new ToolStripMenuItem("Pen", pen);
             ToolStripMenuItem Color = new ToolStripMenuItem("Color");
             ToolStripMenuItem Style = new ToolStripMenuItem("Style");
-            Pen.DropDownItems.Add(Color);
-            Pen.DropDownItems.Add(Style);
-
             ToolStripMenuItem Solid = new ToolStripMenuItem("Solid");
             ToolStripMenuItem Dot = new ToolStripMenuItem("Dot");
             ToolStripMenuItem DashDotDot = new ToolStripMenuItem("DashDotDot");
+            ToolStripMenuItem Help = new ToolStripMenuItem("Help");
+            ToolStripMenuItem About = new ToolStripMenuItem("About", about);
+
+            Pen.DropDownItems.Add(Color);
+            Pen.DropDownItems.Add(Style);
             Style.DropDownItems.Add(Solid);
             Style.DropDownItems.Add(Dot);
             Style.DropDownItems.Add(DashDotDot);
-            ms.Items.Add(Edit);
 
-            ToolStripMenuItem Help = new ToolStripMenuItem("Help");
-            ToolStripMenuItem About = new ToolStripMenuItem("About", about);
             Help.DropDownItems.Add(About);
             About.ShortcutKeys = Keys.F1;
-            About.Click += About_Click;
+
+            New.ShortcutKeys = Keys.Control | Keys.N;
+            Open.ShortcutKeys = Keys.F3;
+            Save.ShortcutKeys = Keys.F2;
+            Exit.ShortcutKeys = Keys.Alt | Keys.X;
+            Undo.ShortcutKeys = Keys.Control | Keys.Z;
+            Redo.ShortcutKeys = Keys.Control | Keys.Shift | Keys.Z;
+
+            File.DropDownItems.Add(New);
+            File.DropDownItems.Add(Open);
+            File.DropDownItems.Add(Save);
+            File.DropDownItems.Add(Exit);
+            ms.Items.Add(File);
+
+            Edit.DropDownItems.Add(Undo);
+            Edit.DropDownItems.Add(Redo);
+            Edit.DropDownItems.Add(Pen);
+            ms.Items.Add(Edit);
+
             ms.Items.Add(Help);
             this.Controls.Add(ms);
+
+            About.Click += About_Click;
+            Save.Click += Save_Click;
+            New.Click += New_Click;
+            Open.Click += Open_Click;
 
             ts = new ToolStrip();
             ts.Items.Add(new_file);
@@ -94,6 +96,8 @@ namespace Laborotornaya_rabota
             ts.Dock = DockStyle.Left;
             this.Controls.Add(ts);
 
+            
+
             panel = new Panel();
             tb = new TrackBar();
             lbl = new Label();
@@ -102,15 +106,14 @@ namespace Laborotornaya_rabota
             panel.BorderStyle= BorderStyle.Fixed3D;
 
             tb.Size = new Size(panel.Width / 3, panel.Height);
-            //tb.Location = new Point(panel.Location.X+tb.Width*2, panel.Location.Y);
+            tb.Location = new Point(514, 0);
 
-            //lbl.Location = new Point(ts.Width, ts.Height-50);
             lbl.Text = tb.Value.ToString();
             tb.Scroll += Tb_Scroll;
             panel.Controls.Add(lbl);
-            panel.Controls.Add(tb);
-            this.Controls.Add(panel);
             
+            this.Controls.Add(panel);
+            panel.Controls.Add(tb);
 
             pb = new PictureBox();
             pb.BorderStyle = BorderStyle.Fixed3D;
@@ -121,11 +124,40 @@ namespace Laborotornaya_rabota
 
             Image pic = new Bitmap(775, 480);
             pb.Image = pic;
+
+            //переместить в pb MouseDown (MouseDown надо создать)
             if (pb.Image == null)
             {
                 MessageBox.Show("You need create a new file!");
                 return;
             }
+        }
+
+        private void New_Click(object? sender, EventArgs e)
+        {
+            if (pb.Image != null)
+            {
+                var result = MessageBox.Show("Сохранить текущее изображение перед созданием нового рисунка?", "Предупреждение", MessageBoxButtons.YesNoCancel);
+                switch (result)
+                {
+                    case DialogResult.No: break;
+                    case DialogResult.Yes: Save_Click(sender, e); break;
+                    case DialogResult.Cancel: return;
+                }
+            }
+        }
+
+        private void Open_Click(object? sender, EventArgs e)
+        {
+            OpenFileDialog OP = new OpenFileDialog();
+            OP.Filter = "PNG Image|*.png |JPEG Image|*.jpg |Bitmap Image|*.bmp |Gif Image|*.gif";
+            OP.Title = "Open an image file";
+            OP.FilterIndex = 1;
+            if(OP.ShowDialog() != DialogResult.Cancel)
+            {
+                pb.Load(OP.FileName);
+            }
+            pb.AutoSize = true;
         }
 
         private void Save_Click(object? sender, EventArgs e)
@@ -134,11 +166,10 @@ namespace Laborotornaya_rabota
             SaveDlg.Filter = "PNG Image|*.png |JPEG Image|*.jpg |Bitmap Image|*.bmp |Gif Image|*.gif";
             SaveDlg.Title = "Save an image file";
             SaveDlg.FilterIndex = 1;
-
-            if (SaveDlg.FileName!="")
+            if (SaveDlg.FileName != "")
             {
                 System.IO.FileStream fs = (System.IO.FileStream)SaveDlg.OpenFile();
-                switch(SaveDlg.FilterIndex)
+                switch (SaveDlg.FilterIndex)
                 {
                     case 1:
                         this.pb.Image.Save(fs, ImageFormat.Png);
@@ -160,11 +191,6 @@ namespace Laborotornaya_rabota
         private void About_Click(object? sender, EventArgs e)
         {
             MessageBox.Show("Avtor: Anton Buivol\nVersion 0.1\nMozhno Nazhimat na knopki", "About", MessageBoxButtons.OK);
-        }
-
-        private void Undo_Click(object? sender, EventArgs e)
-        {
-            MessageBox.Show("rabotaet","work",MessageBoxButtons.OK);
         }
 
         private void Tb_Scroll(object? sender, EventArgs e)
